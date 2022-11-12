101ccd5492cb58838ea8534fbbc34d60a988628c
LAPTOP-38CEQ4VT\xmnis <xmnislas@gmail.com>
2022-11-12 15:18:49 -0700

    ObtenRevistas

--- src/java/acciones/ObtenRevistas.java
+++ src/java/acciones/ObtenRevistas.java
@@ -4,61 +4,76 @@
  */
 package acciones;
 
+import interfaces.IPersistencia;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import objetosNegocio.Revista;
+import objetosTransferencia.Lista;
+import persistencia.PersistenciaBD;
 
 /**
  *
  * @author xmnislas
  */
-public class Control extends HttpServlet {
+public class ObtenRevistas extends HttpServlet {
 
     /**
-     * Procesa las solicitudes para ambos metodos HTTP: GET y POST.
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
      *
-     * @param request Objeto request del servlet
-     * @param response Objeto response del servlet
+     * @param request servlet request
+     * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Obten de la solicitud, la tarea seleccionada del menú de revistas
-        String tareaSeleccionada = request.getParameter("tarea");
-        
-        // Obten el objeto session que contiene a las variables con ámbito de sesion
-        HttpSession session = request.getSession();
         RequestDispatcher rd;
         String siguiente = null;
 
-        // Guarda en la variable de sesion tareaSeleccionada, la tarea seleccionada
-        session.setAttribute("tareaSeleccionada", tareaSeleccionada);
+        // Obten el objeto session que contiene a las variables con ámbito de sesion
+        HttpSession session = request.getSession();
 
-        // establece la pagina JSP o servlet que inicia el caso de uso seleccionado
+        // Obten la tarea seleccionada del atributo tareaSel de la variable session
+        String tareaSeleccionada = (String) session.getAttribute("tareaSeleccionada");
+
+        // Crea el objeto para acceder a la base de datos
+        IPersistencia fachada = new PersistenciaBD();
+
+        // Obtiene la lista de revistas del catalogo de revistas
+        ArrayList<Revista> lista = fachada.consultarRevistas();
+
+        // En este bean de tipo Lista, se almacena la lista de
+        // revistas leídas de la tabla catalogoRevistas de la base de datos biblioteca
+        Lista listaRevistas = new Lista("Lista de todas las Revistas", lista);
+
+        // Se requiere guardar en ambito de sesión por que se va a ocupar
+        // en la solicitud de correccion de falla de validacion del lado
+        // del servidor
+        session.setAttribute("listaRevistas", listaRevistas);
+
         switch (tareaSeleccionada) {
-            case "agregarRevista":
-                siguiente = "capturaIsbn.jsp";
-                break;
             case "actualizarRevista":
-                siguiente = "obtenRevistas";
+                siguiente = "seleccionaRevistaActualizar.jsp";
                 break;
             case "eliminarRevistas":
-                siguiente = "obtenRevistas";
+                siguiente = "seleccionaRevistasEliminar.jsp";
                 break;
             case "listarRevistas":
                 siguiente = "despliegaRevistas.jsp";
                 break;
-            default:
-                break;
         }
 
-        // Redirecciona a la página JSP siguiente
-        request.getRequestDispatcher(siguiente).forward(request, response);
-    
+        // Establece la página JSP o servlet siguiente
+        rd = request.getRequestDispatcher(siguiente);
+        // Redirecciona a la página JSP o servlet siguiente
+        rd.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -97,6 +112,7 @@ public class Control extends HttpServlet {
      */
     @Override
     public String getServletInfo() {
-        return "Servlet de control";
+        return "Servlet que obtiene la lista de revistas del catalogo de revistas";
     }// </editor-fold>
+
 }
